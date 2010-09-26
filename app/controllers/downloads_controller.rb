802004462ca919dae0e90b70ab7e03d341c60b48
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2010-09-26 20:27:51 +0200

    prototype fix

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,5 +1,5 @@
 class DownloadsController < ApplicationController
-  filter_resource_access 
+  filter_resource_access
   def new
     @lecture = Lecture.find(params[:lecture_id])
     @download=@lecture.downloads.build
