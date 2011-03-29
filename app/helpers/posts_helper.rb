16c0dacb19373526c29ce5f0dfbb42b3f3756cbd
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2011-03-29 23:01:06 +0200

    youtube support

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def format_post(text)
+    text=simple_format(auto_link(text))
+    before='<iframe title="YouTube video player" width="550" height="335" src="http://www.youtube.com/embed/'
+    after='" frameborder="0" allowfullscreen></iframe>'
+    text.gsub!('[YouTube]',before)
+    text.gsub!('[/YouTube]',after)
+  end
 end
+
