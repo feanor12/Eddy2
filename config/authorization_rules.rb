d4188615e2e7e229c79056088df16899ab7485ce
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2011-05-16 19:04:48 +0200

    allow download delete to user

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -84,9 +84,9 @@ authorization do
     end
 
     has_permission_on :downloads, :to=>[:read,:create,:download]
-    #has_permission_on :downloads, :to=>[:update,:destroy] do
-    #  if_attribute :user => is {user}
-    #end
+    has_permission_on :downloads, :to=>[:update,:destroy] do
+      if_attribute :user => is {user}
+    end
 
     has_permission_on :comments, :to=>[:create,:read]
     has_permission_on :comments, :to=>[:manage] do
