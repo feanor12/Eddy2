88cf96c9fb0f5ba873ef71163f51eebc74926212
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2014-03-01 13:52:16 +0100

    update youtube embed

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
     text=simple_format(auto_link(text))
     
     #youtube
-    before='<iframe title="YouTube video player" width="544" height="335" src="http://www.youtube.com/embed/'
+    before='<iframe width="544" height="335" src="//www.youtube-nocookie.com/embed/'
     after='" frameborder="0" allowfullscreen></iframe>'
     text.gsub!('[YouTube]',before)
     text.gsub!('[/YouTube]',after)
